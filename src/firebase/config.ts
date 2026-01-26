414c57312d9e561461653023388eefa6aeaed8e1
Pedro Souto <pesoutom@gmail.com>
2026-01-26 15:54:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1340288694-398a0",
+  "appId": "1:538920923900:web:012e1d14c3b7f9ff189d79",
+  "apiKey": "AIzaSyCT809ZZzSWcaulPAmuifU6fj6BZa5XoiM",
+  "authDomain": "studio-1340288694-398a0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "538920923900"
+};
